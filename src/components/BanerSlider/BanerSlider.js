fb56574286e359b9e62c402f1553e5a555066058
Ganesh-Paulraja <ganeshsgk555@gmail.com>
2024-08-25 15:21:21 +0530

    update

--- src/components/BanerSlider/BanerSlider.js
+++ src/components/BanerSlider/BanerSlider.js
@@ -10,7 +10,7 @@ import image2 from '../../images/banners/Frame 4.png'
 import image3 from '../../images/banners/Frame 5.png'
 import arrowRight from '../../images/right-arrow.png'
 import arrowLeft from '../../images/left-arrow.png'
-
+const images = [image1, image2, image3];
 export default function BanerSlider() {
   const swiperRef = useRef(null);
 
@@ -32,6 +32,8 @@ export default function BanerSlider() {
     <div className='slider-wrap'>
       <Swiper
       // install Swiper modules
+      //npm i swiper@9.3.2
+      //npm i swiper-react
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       ref={swiperRef}
       spaceBetween={50}
@@ -47,9 +49,14 @@ export default function BanerSlider() {
       loop={true}
       autoplay={{ delay: 5000 }}
     >
-      <SwiperSlide><img src={image1} alt='banner'/></SwiperSlide>
+      {/* <SwiperSlide><img src={image1} alt='banner'/></SwiperSlide>
       <SwiperSlide><img src={image2} alt='banner'/></SwiperSlide>
-      <SwiperSlide><img src={image3} alt='banner'/></SwiperSlide>
+      <SwiperSlide><img src={image3} alt='banner'/></SwiperSlide> */}
+       {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt={`banner ${index + 1}`} />
+          </SwiperSlide>
+        ))}
     </Swiper>
 
       <div className="swiper-button-next">
